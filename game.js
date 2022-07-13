38a038a4dbb42acf9805d528c2c01c6f38c11b59
Yegor Kochetkov <kegor.fx@gmail.com>
2022-07-13 15:03:33 +0300

    Change adaptation for mobile

--- game.js
+++ game.js
@@ -1,8 +1,12 @@
 window.onload = function () {
   var config = {
     type: Phaser.AUTO,
-    width: 600,
-    height: 900,
+    scale: {
+      mode: Phaser.Scale.FIT,
+      autoCenter: Phaser.Scale.CENTER_BOTH,
+      width: 600,
+      height: 900
+    },
     title: "My Fantasy: Make Your Story",
     scene: [Intro, Tutorial, Scene1, Scene2, Scene3, Scene4],
   }
